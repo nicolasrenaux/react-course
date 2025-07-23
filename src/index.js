dd542d8e495da3b2c1a0fe47d450cb9d6d64cbf7
Nicolas Renaux <nickrenaux@gmmail.com>
2025-07-23 00:18:54 -0300

    :sparkles: feat: learning how lists work!

--- src/index.js
+++ src/index.js
@@ -3,38 +3,46 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 
+const books = [
+  {
+  author: 'Mel Robbins',
+  title: 'The Let Them Theory: A Life-Changing Tool That Millions of People Cant Stop Talking About',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/91ZVf3kNrcL._AC_UL600_SR600,400_.jpg',
+  id: 1,
+},
+{
+  author: 'James Clear',
+  title: 'Atomic Habits',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+  id: 2,
+},
+];
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        // const {img, author, title, id} = book;
+        return (
+          // <Book img = {img} author = {author} title = {title} key={id}/>
+          <Book {...book} key={book.id}/>
+        )
+      })}
     </section>
   )
 }
 
-const author = 'Mel Robbins';
-const Book = () => {
+const Book = (props) => {
   const inlineStyles = {
     color: '#617d98',
     fontSize: '1rem',
   }
 
-  const title = 'The Let Them Theory: A Life-Changing Tool That Millions of People Cant Stop Talking About';
-
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/91ZVf3kNrcL._AC_UL600_SR600,400_.jpg"
-        alt="Interesting Facts For Curious Minds"
-      />
-      <h2 style={inlineStyles}>
-        {title}
-      </h2>
-      <h4>{author}</h4>
+      <img src={props.img} alt={props.title} />
+      <h2 style={inlineStyles}>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   )
 }
