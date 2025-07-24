c3c0a61b2b464cd60ed81fedadde6f18b1c70ab7
Nicolas Renaux <nickrenaux@gmmail.com>
2025-07-24 20:29:13 -0300

    :sparkles: feat: Title and data 'components'

--- 2-backroads-project/src/components/Navbar.js
+++ 2-backroads-project/src/components/Navbar.js
@@ -1,77 +1,47 @@
 import logo from '../images/logo.svg'
+import { pageLinks, socialData } from '../data'
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-        <div className="nav-center">
-          <div className="nav-header">
-            <img src={logo} className="nav-logo" alt="backroads" />
-            <button type="button" className="nav-toggle" id="nav-toggle">
-              <i className="fas fa-bars"></i>
-            </button>
-          </div>
-          {/* <!-- left this comment on purpose --> */}
-          <ul className="nav-links" id="nav-links">
-            <li>
-              <a href="#home" className="nav-link">
-                {' '}
-                home{' '}
-              </a>
-            </li>
-
-            <li>
-              <a href="#about" className="nav-link">
-                {' '}
-                about{' '}
-              </a>
-            </li>
-
-            <li>
-              <a href="#services" className="nav-link">
-                {' '}
-                services{' '}
-              </a>
-            </li>
-
-            <li>
-              <a href="#tours" className="nav-link">
-                {' '}
-                tours
-              </a>
-            </li>
-          </ul>
-
-          <ul className="nav-icons">
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-facebook"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-twitter"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-squarespace"></i>
-              </a>
-            </li>
-          </ul>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
         </div>
-      </nav>
+        {/* <!-- left this comment on purpose --> */}
+        <ul className="nav-links" id="nav-links">
+          {pageLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.href} className="nav-link">
+                  {link.text}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+
+        <ul className="nav-icons">
+          {socialData.map((data) => {
+            return (
+              <li key={data.id}>
+                <a
+                  href="https://www.twitter.com"
+                  target="_blank"
+                  className="nav-icon"
+                  rel="noreferrer"
+                >
+                  <i className={'fab ' + data.iconClass}></i>
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </nav>
   )
 }
 export default Navbar
---
+++ 2-backroads-project/src/data.js
@@ -0,0 +1,20 @@
+export const pageLinks = [
+    {
+        id:1, href: '#home', text: 'home'
+    },
+    {
+        id:2, href: '#about', text: 'about'
+    },
+    {
+        id:3, href: '#services', text: 'services'
+    },
+    {
+        id:4, href: '#tours', text: 'tours'
+    },
+];
+
+export const socialData = [
+    {id: 1, iconClass: 'fa-facebook'},
+    {id: 2, iconClass: 'fa-twitter'},
+    {id: 3, iconClass: 'fa-squarespace'},
+]
