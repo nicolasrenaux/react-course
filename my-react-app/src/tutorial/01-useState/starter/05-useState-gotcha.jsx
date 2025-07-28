081189fd6928280b2edfbdf4d058a5397f0cbf35
Nicolas <105075612+nicolasrenaux@users.noreply.github.com>
2025-07-28 16:29:13 -0300

    :sparkles: feat: finished useState learning

--- my-react-app/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ my-react-app/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+
+  const [value, setValue] = useState(0);
+  // Abaixo pega o valor atual do estado e não o anterior..
+  // const handleClick = () =>{
+  //   setValue((currentState)=>{
+  //     const newState = currentState +1;
+  //     console.log(newState);
+      
+  //     return newState;
+  //   });
+  // }
+
+  const handleClick = () =>{
+    setTimeout(()=>{
+      console.log("Click the button");
+      
+      setValue((currentState)=>{
+        const newState = currentState + 1;
+        return newState;
+      });
+    }, 3000)
+  }
+
+  return <div>
+    <h1>{value}</h1>
+    <button className="btn" onClick={handleClick}>Increase</button>
+  </div>;
 };
 
 export default UseStateGotcha;
