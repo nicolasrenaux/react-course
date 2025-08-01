9fb4a1ccbe07d4228c2cb2982d5d4cb9cac64912
Nicolas <105075612+nicolasrenaux@users.noreply.github.com>
2025-08-01 14:02:30 -0300

    :sparkles: feat: finished tabs project

--- 04-fundamental-projects/06-tabs/starter/src/App.jsx
+++ 04-fundamental-projects/06-tabs/starter/src/App.jsx
@@ -1,6 +1,41 @@
+import { useState, useEffect } from "react";
+import JobInfo from "./JobInfo";
+import BtnContainer from "./BtnContainer";
 const url = 'https://www.course-api.com/react-tabs-project';
 
 const App = () => {
-  return <h2>Tabs Starter</h2>;
+  const [jobs, setJobs] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [ currentItem, setCurrentItem ] = useState(0);
+
+  const fetchData = async () =>{
+    const res = await fetch(url);
+    if(!res.ok){
+      return res.status(400).json({erro:"Não foi possível realizar o fetch."})
+    }
+    const data = await res.json();
+    setJobs(data);
+    setIsLoading(false)  
+  }
+
+  useEffect(()=>{
+    fetchData();
+  }, [])
+
+  if(isLoading){
+    return(
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    )
+  }
+  
+
+  return(
+    <section className="jobs-center">
+      <BtnContainer jobs={jobs} setCurrentItem={setCurrentItem} currentItem={currentItem}/>
+      <JobInfo jobs={jobs} currentItem={currentItem}/>
+    </section>
+  );
 };
 export default App;
---
+++ 04-fundamental-projects/06-tabs/starter/src/JobInfo.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Duties from './Duties';
+
+const JobInfo = ({jobs, currentItem}) => {
+
+    const activeId = jobs.id;
+
+    const {company, dates, duties, title} = jobs[currentItem];
+    
+  return (
+    <article className='job-info'>
+        <h3>{title}</h3>
+        <span className='job-company'>{company}</span>
+        <p className='job-date'>{dates}</p>
+        <Duties duties={duties}/>
+    </article>
+  )
+}
+
+export default JobInfo
